7b8ac31fd51f58dd6fca51f7d33b57c8cbb6dbaf
iraj259 <irajj.259@gmail.com>
2025-11-03 21:51:04 +0000

    Added get flight api

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,16 +1,19 @@
 const axios = require('axios')
 const {BookingRepository} = require('../repositories')
+const {StatusCodes} = require('http-status-codes')
 const {ServerConfig} = require('../config')
 const db = require('../models')
 async function createBooking(data){
     try {
         const result = db.sequelize.transaction(async function bookingImpl(t){
             const flight= await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`)
-            console.log(flight.data)
+            if(data.noofSeats > flight.totalSeats){
+            throw new AppError("Not enough seats available",StatusCodes.INTERNAL_SERVER_ERROR)
+            }
             return true
         })
     } catch (error) {
-        
+console.log(error)
     }
 }
 
