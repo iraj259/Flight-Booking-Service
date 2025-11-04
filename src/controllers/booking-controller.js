76e974c6c03cb95540f51b10f72d69aa6a57af53
iraj259 <irajj.259@gmail.com>
2025-11-04 22:19:29 +0000

    Added cancel booking logic with cron

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,7 +1,8 @@
 const { BookingService } = require("../services");
 const { SuccessResponse, ErrorResponse } = require("../utils/common");
 const { StatusCodes } = require("http-status-codes");
-
+const { message } = require("../utils/common/error-response");
+const inMemDb = {}
 async function createBooking(req, res) {
   try {
     const response = await BookingService.createBooking({
@@ -28,12 +29,23 @@ async function createBooking(req, res) {
 
 async function makePayment(req, res) {
   try {
+    const idempotencyKey = req.headers['x-idempotency-key']
+    if(!idempotencyKey){
+      return res
+       .status(StatusCodes.BAD_REQUEST)
+      .json({message:'idempotency key missing'});
+    }
+    if(!idempotencyKey || inMemDb[idempotencyKey]){
+      return res
+       .status(StatusCodes.BAD_REQUEST)
+      .json({message:'cannot retry on a successful pay'});
+    }
     const response = await BookingService.makePayment({
       totalCost: req.body.totalCost,
       userId: req.body.userId,
       bookingId: req.body.bookingId, 
     });
-
+inMemDb[idempotencyKey] = idempotencyKey
     SuccessResponse.data = response;
     return res.status(StatusCodes.OK).json(SuccessResponse);
   } catch (error) {
